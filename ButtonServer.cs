fb770f83eb21e597ca41e6d8e78926039ab04e0b
liso4ka <bafometelize@gmail.com>
2023-05-13 11:26:28 -0700

    some fixes

---
+++ ButtonServer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace course
+{
+    internal class ButtonServer : NetworkButton
+    {
+        public ButtonServer()
+        {
+            Image = course.Properties.Resources.server;
+        }
+    }
+}
