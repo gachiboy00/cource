76c1a56123bf30d6e2080e810e7818b1de42b2c1
aboba <gachiboy00@mail.ru>
2023-05-13 21:02:40 +0700

    v1
    Кнопки без ip и dns

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace course
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.conbtn = new System.Windows.Forms.Button();
+            this.serverbtn = new System.Windows.Forms.Button();
+            this.routerbtn = new System.Windows.Forms.Button();
+            this.btnPC = new System.Windows.Forms.Button();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.serverbtn);
+            this.panel1.Controls.Add(this.routerbtn);
+            this.panel1.Controls.Add(this.btnPC);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(808, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(150, 488);
+            this.panel1.TabIndex = 0;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(3, 29);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(3, 6);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.radioButton4);
+            this.panel2.Controls.Add(this.radioButton1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.radioButton3);
+            this.panel2.Controls.Add(this.conbtn);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.radioButton2);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 123);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(146, 250);
+            this.panel2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(2, 174);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(146, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "№ Элемента";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(2, 141);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(146, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(2, 121);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // conbtn
+            // 
+            this.conbtn.Location = new System.Drawing.Point(2, 98);
+            this.conbtn.Name = "conbtn";
+            this.conbtn.Size = new System.Drawing.Size(146, 23);
+            this.conbtn.TabIndex = 3;
+            this.conbtn.Text = "Соеденить";
+            this.conbtn.UseVisualStyleBackColor = true;
+            this.conbtn.Click += new System.EventHandler(this.conbtn_Click);
+            // 
+            // serverbtn
+            // 
+            this.serverbtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.serverbtn.Image = global::course.Properties.Resources.server;
+            this.serverbtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.serverbtn.Location = new System.Drawing.Point(0, 82);
+            this.serverbtn.Name = "serverbtn";
+            this.serverbtn.Size = new System.Drawing.Size(146, 41);
+            this.serverbtn.TabIndex = 2;
+            this.serverbtn.Text = "Server";
+            this.serverbtn.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.serverbtn.UseVisualStyleBackColor = true;
+            this.serverbtn.Click += new System.EventHandler(this.serverbtn_Click);
+            // 
+            // routerbtn
+            // 
+            this.routerbtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.routerbtn.Image = global::course.Properties.Resources.router;
+            this.routerbtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.routerbtn.Location = new System.Drawing.Point(0, 41);
+            this.routerbtn.Name = "routerbtn";
+            this.routerbtn.Size = new System.Drawing.Size(146, 41);
+            this.routerbtn.TabIndex = 1;
+            this.routerbtn.Text = "Router";
+            this.routerbtn.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.routerbtn.UseVisualStyleBackColor = true;
+            this.routerbtn.Click += new System.EventHandler(this.routerbtn_Click);
+            // 
+            // btnPC
+            // 
+            this.btnPC.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPC.Image = global::course.Properties.Resources.pc;
+            this.btnPC.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPC.Location = new System.Drawing.Point(0, 0);
+            this.btnPC.Name = "btnPC";
+            this.btnPC.Size = new System.Drawing.Size(146, 41);
+            this.btnPC.TabIndex = 0;
+            this.btnPC.Text = "PC";
+            this.btnPC.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnPC.UseVisualStyleBackColor = true;
+            this.btnPC.Click += new System.EventHandler(this.btnPC_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(3, 52);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(3, 75);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(85, 17);
+            this.radioButton4.TabIndex = 8;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(958, 488);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button serverbtn;
+        private System.Windows.Forms.Button routerbtn;
+        private System.Windows.Forms.Button btnPC;
+        private System.Windows.Forms.Button conbtn;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Button = System.Windows.Forms.Button;
+
+namespace course
+{
+    public partial class Form1 : Form
+    {
+        private bool _isDragging = false;
+        private Point _lastMousePos;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPC_Click(object sender, EventArgs e)
+        {
+            Button PCbtn = new Button();
+            PCbtn.Image = course.Properties.Resources.pc;
+            PCbtn.ImageAlign = ContentAlignment.MiddleCenter;
+            PCbtn.Height = 40;
+            PCbtn.Width = 40;
+            int x = 100;
+            int y = 100;
+            PCbtn.Location = new Point(x, y);
+
+            PCbtn.MouseDown += new MouseEventHandler(btn_MouseDown);
+            PCbtn.MouseMove += new MouseEventHandler(btn_MouseMove);
+            PCbtn.MouseUp += new MouseEventHandler(btn_MouseUp);
+            PCbtn.MouseDown += new MouseEventHandler(btn_RightClik);
+
+            this.Controls.Add(PCbtn);
+        }
+
+
+        private void routerbtn_Click(object sender, EventArgs e)
+        {
+            Button routerbtn = new Button();
+            routerbtn.Image = course.Properties.Resources.router;
+            routerbtn.ImageAlign = ContentAlignment.MiddleCenter;
+            routerbtn.Height = 40;
+            routerbtn.Width = 40;
+            int x = 100;
+            int y = 100;
+            routerbtn.Location = new Point(x, y);
+
+            routerbtn.MouseDown += new MouseEventHandler(btn_MouseDown);
+            routerbtn.MouseMove += new MouseEventHandler(btn_MouseMove);
+            routerbtn.MouseUp += new MouseEventHandler(btn_MouseUp);
+            routerbtn.MouseDown += new MouseEventHandler(btn_RightClik);
+
+            this.Controls.Add(routerbtn);
+        }
+
+        private void serverbtn_Click(object sender, EventArgs e)
+        {
+            Button serverbtn = new Button();
+            serverbtn.Image = course.Properties.Resources.server;
+            serverbtn.ImageAlign = ContentAlignment.MiddleCenter;
+            serverbtn.Height = 40;
+            serverbtn.Width = 40;
+            int x = 100;
+            int y = 100;
+            serverbtn.Location = new Point(x, y);
+
+            serverbtn.MouseDown += new MouseEventHandler(btn_MouseDown);
+            serverbtn.MouseMove += new MouseEventHandler(btn_MouseMove);
+            serverbtn.MouseUp += new MouseEventHandler(btn_MouseUp);
+            serverbtn.MouseDown += new MouseEventHandler(btn_RightClik);            
+
+            this.Controls.Add(serverbtn);
+        }
+
+        
+        private void btn_MouseDown(object sender, MouseEventArgs e)
+        {
+            _isDragging = true;
+            _lastMousePos = e.Location;
+            
+            if (e.Button == MouseButtons.Left)
+            {
+                Control clickedControl = sender as Control;
+                int controlIndex = this.Controls.IndexOf(clickedControl);
+                // Do something with controlIndex
+                textBox3.Text = controlIndex.ToString();
+            }
+        }
+
+        private void btn_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isDragging)
+            {
+                int deltaX = e.X - _lastMousePos.X;
+                int deltaY = e.Y - _lastMousePos.Y;
+
+
+                for (int i = 0; i < this.Controls.Count; ++i)
+                {
+                    if (sender == this.Controls[i])
+                    {
+                        this.Controls[i].Left += deltaX;
+                        this.Controls[i].Top += deltaY;
+                    }
+                }
+                _lastMousePos = e.Location;
+            }
+        }
+
+        private void btn_MouseUp(object sender, MouseEventArgs e)
+        {
+            _isDragging = false;
+        }
+
+        private void btn_RightClik(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                // Создаем новое контекстное меню и добавляем пункты меню
+                ContextMenuStrip contextMenuStrip = new ContextMenuStrip();
+                ToolStripMenuItem item1 = new ToolStripMenuItem("Delete");
+                contextMenuStrip.Items.Add(item1);
+
+                // Добавляем обработчик события Click для пункта меню "Delete"
+                item1.Click += (s, ev) =>
+                {
+                    // Удаляем кнопку из контейнера элементов управления
+                    this.Controls.Remove(sender as Button);
+                };
+
+                // Отображаем контекстное меню
+                contextMenuStrip.Show(sender as Control, e.Location);
+            }
+        }
+
+
+        
+        private void conbtn_Click(object sender, EventArgs e)
+        {
+            //// Получаем координаты первой кнопки
+            //int x1 = this.Controls[int.Parse(textBox1.Text)].Location.X + this.Controls[int.Parse(textBox1.Text)].Width / 2;
+            //int y1 = this.Controls[int.Parse(textBox1.Text)].Location.Y + this.Controls[int.Parse(textBox1.Text)].Height / 2;
+
+            //// Получаем координаты второй кнопки
+            //int x2 = this.Controls[int.Parse(textBox2.Text)].Location.X + this.Controls[int.Parse(textBox2.Text)].Width / 2;
+            //int y2 = this.Controls[int.Parse(textBox2.Text)].Location.Y + this.Controls[int.Parse(textBox2.Text)].Height / 2;
+
+            //// Создаем поверхность для рисования
+            //Graphics g = this.CreateGraphics();
+
+            //if (radioButton1.Checked)
+            //{
+            //    _pen.DashStyle = DashStyle.Solid;
+            //    _pen.Color = Color.Black;
+            //    g.DrawLine(_pen, x1, y1, x2, y2);
+            //}
+
+
+            //// Рисуем линию между кнопками
+            ////g.DrawLine(_pen, x1, y1, x2, y2);
+            
+            if (int.TryParse(textBox1.Text, out int index1) && int.TryParse(textBox2.Text, out int index2))
+            {
+                // Проверяем, что индексы действительно представляют существующие контролы на форме
+                if (index1 >= 0 && index1 < this.Controls.Count && index2 >= 0 && index2 < this.Controls.Count)
+                {
+                    // Получаем координаты первой кнопки
+                    int x1 = this.Controls[index1].Location.X + this.Controls[index1].Width / 2;
+                    int y1 = this.Controls[index1].Location.Y + this.Controls[index1].Height / 2;
+
+                    // Получаем координаты второй кнопки
+                    int x2 = this.Controls[index2].Location.X + this.Controls[index2].Width / 2;
+                    int y2 = this.Controls[index2].Location.Y + this.Controls[index2].Height / 2;
+
+                    // Создаем поверхность для рисования
+                    using (Graphics g = this.CreateGraphics())
+                    {
+                        if (radioButton1.Checked)
+                        {
+                            // Создаем объект Pen со стилем Solid и цветом Black
+                            using (Pen pen = new Pen(Color.Black,3.0f))
+                            {
+                                pen.DashStyle= DashStyle.Solid;
+                                g.DrawLine(pen, x1, y1, x2, y2);
+                            }
+                        }
+                        if (radioButton2.Checked)
+                        {
+                            // Создаем объект Pen со стилем Dot и цветом Black
+                            using (Pen pen = new Pen(Color.Black, 3.0f))
+                            {
+                                pen.DashStyle = DashStyle.Dot;
+                                g.DrawLine(pen, x1, y1, x2, y2);
+                            }
+                        }
+                        if (radioButton3.Checked)
+                        {
+                            // Создаем объект Pen со стилем DashDot и цветом Black
+                            using (Pen pen = new Pen(Color.Black, 3.0f))
+                            {
+                                pen.DashStyle = DashStyle.DashDot;
+                                g.DrawLine(pen, x1, y1, x2, y2);
+                            }
+                        }
+                        if (radioButton4.Checked)
+                        {
+                            // Создаем объект Pen со стилем DashDotDot и цветом Black
+                            using (Pen pen = new Pen(Color.Black, 3.0f))
+                            {
+                                pen.DashStyle = DashStyle.DashDotDot;
+                                g.DrawLine(pen, x1, y1, x2, y2);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Неверный индекс контрола.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите корректный индекс контрола.");
+            }
+        }
+    }
+}
